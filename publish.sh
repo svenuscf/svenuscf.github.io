d2f2e592d151784f6d69135ef4db73acb483774b
svenuscf <ajidom@gmail.com>
2026-07-07 07:23:19 +1000

    Publish new post

--- publish.sh
+++ publish.sh
@@ -7,6 +7,6 @@ cp public/sitemap.xml .
 
 cp -R public/posts/* posts/
 cp -R public/tags/* tags/
-cp -R public/categories/* categories/」
+cp -R public/categories/* categories/
 cp -R public/about/* about/ 2>/dev/null || true
 cp -R public/page/* page/ 2>/dev/null || true
